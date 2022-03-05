d8b4f173a3efcb27e23d547d61c75aa328dd43ac
Sergey Kalashnikov <cyborgx999@gmail.com>
2022-03-05 21:08:21 +0200

    Add assets adjust navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,43 @@
+import { ReactComponent as Close } from 'assets/icons/close.svg';
+import IconComponent from 'components/icon';
 import React from 'react';
+import { unathorizedPath } from 'routes/pathKeys';
+import { removeSlash } from 'utils';
 
-import { CloseIcon, NavigationContainer } from './styles';
+import {
+  LinkText,
+  NavigationContainer,
+  NavigationLinkContainer,
+  NavigationTopPart,
+  StyledNavLink,
+} from './styles';
 import { INavbarProps } from './types';
+
+const getNavLinks = (userRole?: 'student' | 'teacher') => {
+  switch (userRole) {
+    default:
+      return unathorizedPath;
+  }
+};
 
 const Navbar = ({ isShown, onToggle }: INavbarProps) => {
   return (
     <NavigationContainer $isShown={isShown}>
-      Navbar
-      <CloseIcon onClick={onToggle} />
+      <NavigationTopPart>
+        <p>Navigation</p>
+        <IconComponent title='Close navbar' onClick={onToggle} Svg={Close} />
+      </NavigationTopPart>
+      <NavigationLinkContainer>
+        {Object.values(getNavLinks()).map((link) => (
+          <StyledNavLink key={link} to={link}>
+            {({ isActive }) => (
+              <LinkText $isActive={isActive}>
+                {link === '/' ? 'Home' : removeSlash(link)}
+              </LinkText>
+            )}
+          </StyledNavLink>
+        ))}
+      </NavigationLinkContainer>
     </NavigationContainer>
   );
 };
--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -1,27 +1,56 @@
-import { ReactComponent as Close } from 'assets/icons/close.svg';
-import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
 export const NavigationContainer = styled.nav<{ $isShown: boolean }>`
   position: absolute;
+  display: flex;
+  flex-direction: column;
   height: 100%;
   width: 10rem;
-  background-color: yellow;
+  background-color: ${({ theme }) => theme.colors.gray};
   left: 0;
   transform: ${({ $isShown }) =>
     $isShown ? 'translate(0, 0)' : 'translate(-100%, 0)'};
   transition: transform 0.5s ease-out;
+  padding: 1rem;
 `;
 
-export const CloseIcon = styled(Close)`
-  width: 1.5rem;
-  height: 1.5rem;
-  path {
-    fill: ${({ theme }) => theme.colors.primary};
+export const NavigationTopPart = styled.div`
+  display: flex;
+  height: max-content;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NavigationLinkContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  flex: 1;
+  justify-content: center;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  text-transform: capitalize;
+  width: max-content;
+`;
+
+export const LinkText = styled.span<{ $isActive: boolean }>`
+  padding: 0.25rem 0.5rem;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.white};
   }
 
-  & :hover {
-    path {
-      fill: ${({ theme }) => theme.colors.primaryHover};
-    }
-  }
+  ${({ $isActive, theme }) =>
+    $isActive &&
+    css`
+      background-color: ${theme.colors.primary};
+      color: ${theme.colors.white};
+    `}
 `;
