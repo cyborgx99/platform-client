91420d8bc3b5556be0dd766d2e19dcc849951f3c
Sergey Kalashnikov <cyborgx999@gmail.com>
2022-03-13 22:55:41 +0200

    Add forgot password page

--- src/pages/forgotPassword/index.tsx
+++ src/pages/forgotPassword/index.tsx
@@ -1,8 +1,41 @@
+import ButtonComponent from 'components/button';
+import FormInput from 'components/input';
 import PageLayout from 'components/page';
+import { Formik } from 'formik';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { HeaderThree, StyledForm } from './styles';
+
+const initialValues = {
+  email: '',
+};
 
 const ForgotPasswordPage = () => {
-  return <PageLayout>ForgotPasswordPage</PageLayout>;
+  const { t } = useTranslation();
+  const handleSendResetPasswordLink = () => {
+    console.log(1);
+  };
+
+  return (
+    <PageLayout>
+      <Formik
+        onSubmit={handleSendResetPasswordLink}
+        initialValues={initialValues}>
+        <StyledForm>
+          <HeaderThree>Enter your email to reset your password.</HeaderThree>
+          <FormInput type='email' name='email' label={t('pages.auth.email')} />
+          <ButtonComponent
+            width='full'
+            isLoading={false}
+            type='submit'
+            variant='primary'>
+            {t('pages.auth.resetButton')}
+          </ButtonComponent>
+        </StyledForm>
+      </Formik>
+    </PageLayout>
+  );
 };
 
 export default ForgotPasswordPage;
---
+++ src/pages/forgotPassword/styles.ts
@@ -0,0 +1,11 @@
+import { Form } from 'formik';
+import styled from 'styled-components';
+import { HeaderThreeBase } from 'styles/globalStyles';
+
+export const StyledForm = styled(Form)`
+  padding: 0.5rem;
+`;
+
+export const HeaderThree = styled(HeaderThreeBase)`
+  margin: 1rem 0;
+`;
