59daba30f80912069db2332107ebd7b7ef8e2d61
Sergey Kalashnikov <cyborgx999@gmail.com>
2022-03-14 15:16:45 +0200

    Add show success message component

---
+++ src/components/success/styles.ts
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components';
+
+export const IconContainer = styled.div`
+  width: 7.5rem;
+  height: 7.5rem;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.green.base};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const iconStyles = css`
+  width: 5rem;
+  height: 5rem;
+  path {
+    fill: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const SuccessContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  height: 100%;
+  text-align: center;
+`;
---
+++ src/components/success/types.ts
@@ -0,0 +1,6 @@
+export interface SuccessComponentProps {
+  title: string;
+  text: string;
+  buttonText: string;
+  onContinue: () => void;
+}
