17b152774b0e1012291ad338f501418fbb61f899
Sergey Kalashnikov <cyborgx999@gmail.com>
2022-03-20 22:29:00 +0200

    Create tab templates

---
+++ src/pages/dashboard/tabs/classroom/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ClassroomTab = () => {
+  return <div>ClassroomTab</div>;
+};
+
+export default ClassroomTab;
---
+++ src/pages/dashboard/tabs/payment/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PaymentTab = () => {
+  return <div>PaymentTab</div>;
+};
+
+export default PaymentTab;
