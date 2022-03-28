37332de0e9af4e6ec6f95d9905cdca13e021f197
Sergey Kalashnikov <cyborgx999@gmail.com>
2022-03-28 22:01:51 +0300

    Update toggle button and its types

--- src/components/toggleButton/types.ts
+++ src/components/toggleButton/types.ts
@@ -1,13 +1,13 @@
 import { Dispatch, SetStateAction } from 'react';
 
-export interface IToggleButtonProps {
-  onSetValue: Dispatch<SetStateAction<string>>;
-  currentValue: string;
-  options: string[];
+export interface IToggleButtonProps<T extends string> {
+  onSetValue: Dispatch<SetStateAction<T>>;
+  currentValue: T;
+  options: T[];
 }
 
-export interface IToggleButtonPartProps {
-  onSetValue: (value: string) => void;
-  currentValue: string;
-  value: string;
+export interface IToggleButtonPartProps<T extends string> {
+  onSetValue: (value: T) => void;
+  currentValue: T;
+  value: T;
 }
