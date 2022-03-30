14d345a3fe36fc35bd7b697dabe7657230b95ab6
Sergey Kalashnikov <cyborgx999@gmail.com>
2022-03-30 21:51:46 +0300

    Add nonformik input

--- src/components/input/types.ts
+++ src/components/input/types.ts
@@ -4,3 +4,15 @@ export interface IFormInputProps {
   type: 'date' | 'datetime-local' | 'email' | 'password' | 'text' | 'url';
   imagePreview?: boolean;
 }
+
+export interface IRegularInputProps {
+  value: string | number;
+  title: string;
+  placeholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  Svg?: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & {
+      title?: string | undefined;
+    }
+  >;
+}
